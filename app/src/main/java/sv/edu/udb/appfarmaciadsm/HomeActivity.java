69373db761f1a476f9152f5a6cbbfddaf7fd0729
Oscar Guillermo Serpas Osegueda <platinumsoul98@hotmail.com>
2021-04-11 19:33:35 -0600

    Módulo de compras

--- app/src/main/java/sv/edu/udb/appfarmaciadsm/HomeActivity.java
+++ app/src/main/java/sv/edu/udb/appfarmaciadsm/HomeActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -28,6 +29,8 @@ public class HomeActivity extends AppCompatActivity {
     medAdapter adapter;
     public static FirebaseDatabase database = FirebaseDatabase.getInstance();
     public static DatabaseReference refMedicinas = database.getReference("medicinas");
+    public static double total;
+    public static int productos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +38,17 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         /*GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
         if(signInAccount!=null)
-        {
+        { }*/
 
-        }*/
-
-        //Asignando método al boton
+        //Asignando método a los botones
         findViewById(R.id.btnLogOff).setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {   LogOff();  }
+        });
+
+        findViewById(R.id.btnComprar).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {   Buy();  }
         });
 
         recView = findViewById(R.id.rcView);
@@ -72,5 +78,15 @@ public class HomeActivity extends AppCompatActivity {
         FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    private void Buy()
+    {
+        if(total > 0)
+        {
+            Intent intent = new Intent(HomeActivity.this, OrderActivity.class);
+            startActivity(intent);
+        }
+        else Toast.makeText(getApplicationContext(), "Ingrese un total válido.", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/sv/edu/udb/appfarmaciadsm/medAdapter.java
+++ app/src/main/java/sv/edu/udb/appfarmaciadsm/medAdapter.java
@@ -1,9 +1,9 @@
 package sv.edu.udb.appfarmaciadsm;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,6 +17,10 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class medAdapter extends FirebaseRecyclerAdapter<Medicina, medAdapter.medViewHolder>
 {
+    public int[] prodCounter = new int[10];
+    public double[] prices = new double[10];
+    public int counter = 0;
+    String price = "0";
     public medAdapter(@NonNull FirebaseRecyclerOptions<Medicina> options) {
         super(options);
     }
@@ -24,8 +28,21 @@ public class medAdapter extends FirebaseRecyclerAdapter<Medicina, medAdapter.med
     @Override
     protected void onBindViewHolder(@NonNull medViewHolder holder, int position, @NonNull Medicina model)
     {
+        //Seteando los campos de cada medicina
+        //Nombre
         holder.txtNombre.setText(model.getNombre());
-        holder.txtPrecio.setText("$" + String.valueOf(model.getPrecio()));
+        //Precio
+        price = String.valueOf(model.getPrecio());
+        if(price.substring(price.lastIndexOf(".") + 1).length() < 2)
+        {
+            //Se añade un 0 para uniformidad
+            price = price + "0";
+        }
+        holder.txtPrecio.setText("$" + price);
+        if(counter > 9) counter = 9;
+        prices[counter] = model.getPrecio();
+        counter = counter + 1;
+        //Imagen
         Glide.with(holder.imgView.getContext()).load(model.getImgUrl()).into(holder.imgView);
     }
 
@@ -40,12 +57,41 @@ public class medAdapter extends FirebaseRecyclerAdapter<Medicina, medAdapter.med
     class medViewHolder extends RecyclerView.ViewHolder
     {
         CircleImageView imgView;
-        TextView txtNombre, txtPrecio;
+        TextView txtNombre, txtPrecio, txtCantidad;
         public medViewHolder(@NonNull View itemView) {
             super(itemView);
             imgView = itemView.findViewById(R.id.img1);
             txtNombre = itemView.findViewById(R.id.nameText);
             txtPrecio = itemView.findViewById(R.id.priceText);
+            txtCantidad = itemView.findViewById(R.id.cantidadText);
+
+            //Asignando métodos a los botones
+            itemView.findViewById(R.id.btnAñadir).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    HomeActivity.total = HomeActivity.total + prices[getAdapterPosition()];
+                    prodCounter[getAdapterPosition()] = prodCounter[getAdapterPosition()] + 1;
+                    txtCantidad.setText(String.valueOf(prodCounter[getAdapterPosition()]));
+
+                    Log.d("a", String.valueOf(prodCounter[getAdapterPosition()]));
+                    Log.d("a", String.valueOf(HomeActivity.total));
+                }
+            });
+
+            itemView.findViewById(R.id.btnQuitar).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(HomeActivity.total - prices[getAdapterPosition()] >= 0 && prodCounter[getAdapterPosition()] - 1 >= 0)
+                    {
+                        HomeActivity.total = HomeActivity.total - prices[getAdapterPosition()];
+                        prodCounter[getAdapterPosition()] = prodCounter[getAdapterPosition()] - 1;
+                        txtCantidad.setText(String.valueOf(prodCounter[getAdapterPosition()]));
+
+                        Log.d("a", String.valueOf(prodCounter[getAdapterPosition()]));
+                        Log.d("a", String.valueOf(HomeActivity.total));
+                    }
+                }
+            });
         }
     }
 }
